87f172dde9f947e25a0879c952a761614c3e4d56
unknown <tansenananda@gmail.com>
2019-06-19 23:21:22 +0600

    mid

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 /* Add your script here */
-var input1 = getElementByName("letter1");
-var input2 = getElementByName("letter2")
- 
+var input1 = document.getElementById("letter1");
+var input2 = document.getElementById("letter2")
+var radius = document.getElementById("radius");
+
+
+console.log(input1.innerText)
+
 $(document).ready(function(){
-    $('#roundBtn').click(function(
-        $('#inp').text(input1 + input2);
-    ))
+    $('#roundBtn').click(function(event){
+       
+    document.getElementById('result').value = (input1.value + input2.value);
+    $("#result").css('border-radius',  parseInt(radius.value));
+    })
 });
 
 
+
+    
+    
+    function changeBG(event){
+        if(event.target.innerText == 'V'){
+        //var color = event.target.innerText.toLowerCase()
+    $('#result').css('background-color', 'violet')}
+
+
+    else if(event.target.innerText == 'I')
+    {
+        $('#result').css('background-color', 'indigo');
+    }
+
+    else if(event.target.innerText == 'R')
+    {
+        $('#result').css('background-color', 'red');
+        
+    }
+    else if(event.target.innerText == 'B')
+    {
+        $('#result').css('background-color', 'blue');
+        
+    }
+    else if(event.target.innerText == 'W')
+    {
+        $('#result').css('background-color', 'white');
+        
+    }
+    }   
+
+
+    
