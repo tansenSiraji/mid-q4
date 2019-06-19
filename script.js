6c3b9ce5ab99fa8a094160d460720f4a02bc8827
cis101 <tansenananda@gmail.com>
2019-06-19 09:43:03 +0600

    mid

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 /* Add your script here */
-
+var input1 = getElementByName("letter1");
+var input2 = getElementByName("letter2")
  
+$(document).ready(function(){
+    $('#roundBtn').click(function(
+        $('#inp').text(input1 + input2);
+    ))
+});
 
 
